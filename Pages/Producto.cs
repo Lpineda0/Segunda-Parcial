189a146f2358b69689ae151a27871da9e9eeead2
EL_MI <EL_MI@LAPTOP-9J93HQG4>
2024-04-17 13:12:39 -0600

    Agregar archivos de proyecto.

---
+++ Pages/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BlazorApp5.Pages
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Departamento { get; set; } = "";
+
+    }
+}
